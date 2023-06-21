ef098d95b8a86a8e1d371a38535f8f1c0bddca03
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-06-21 20:51:28 +0100

    Installed Redux Dependencies

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Trying to Build Uber.</Text>
       <StatusBar style="auto" />
     </View>
   );

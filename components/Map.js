60464fe0fef181ba58a0ebaa40e5a3184f1621b2
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-06-22 22:52:14 +0100

    Map finalized

--- components/Map.js
+++ components/Map.js
@@ -3,14 +3,19 @@ import MapView from "react-native-maps";
 import React from "react";
 import { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slice/navSlice";
 
 const Map = () => {
+const origin = useSelector(selectOrigin)
+
   return (
     <MapView
     style={tw`flex-1`}
+    mapType="mutedStandard"
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
